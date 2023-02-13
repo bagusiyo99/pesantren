a676600f4a7e63e35e9013caa9714b211a010968
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-02-13 10:35:13 +0700

    perbaikan

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -36,36 +36,56 @@
     </nav>
     
 </header> --}}
-<section id="topbar "
-    class="mb-2 mb-lg-0 mb-sm-0 d-none d-lg-flex align-items-center pt-2 pb-2 bg-success text-white topbar-transparent">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-success">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6   judul">
-                <span class="px-3">Bagus Cell |</span>
-                <span class="px-1">08943943xx </span>
-
-
-            </div>
-            <div class="col-md-6 text-end">
-                <a href="" class="me-3 icon">
-                    <i class="fa-brands fa-youtube"></i>
-                </a>
-                <a href="" class="me-3 icon">
-                    <i class="fa-brands fa-twitter"></i>
-                </a>
-                <a href="" class="me-3 icon">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-                <a href="" class="me-3 icon">
-                    <i class="fa-brands fa-facebook"></i> </a>
-
-            </div>
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('home') ? 'menu-active' : '' }}" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  {{ Request::is('about') ? 'menu-active' : '' }}" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  {{ Request::is('blog') ? 'menu-active' : '' }}" href="/blog">Blog</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Foto
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Foto
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  {{ Request::is('contact') ? 'menu-active' : '' }}" href="/contact">Contact</a>
+                </li>
+            </ul>
         </div>
     </div>
-</section>
+</nav>
 
 
-<header>
+{{-- <header>
 
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed">
@@ -119,7 +139,7 @@
             </div>
         </div>
     </nav>
-</header>
+</header> --}}
 
 
 {{-- <!--Navbar Start-->
