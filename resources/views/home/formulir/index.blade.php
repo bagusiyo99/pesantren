d28a8e256788a024859ccc9c5c8638cddc3cc31a
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-02-21 23:14:04 +0700

    perbaikan

--- resources/views/home/formulir/index.blade.php
+++ resources/views/home/formulir/index.blade.php
@@ -36,3 +36,46 @@
             </form>
         </div>
     </div>
+
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni enim tenetur, aut non
+                            consectetur facilis! Iusto quis recusandae sed exercitationem. Sequi, ab saepe. In dolor
+                            voluptas repellat aliquam nemo ex. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            Autem ipsum repellendus at facilis culpa quis rerum nemo ipsam non. Nam laborum iusto harum
+                            molestias a quaerat laudantium itaque excepturi maiores!</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <ul class="list-group col-sm-14">
+            <li class="list-group-item active" aria-current="true">An active item</li>
+            <li class="list-group-item">A second item Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nulla
+                labore corrupti dicta et veniam optio harum voluptas quibusdam officiis, enim hic porro vero illum
+                necessitatibus velit provident architecto blanditiis. Lorem ipsum dolor sit amet consectetur,
+                adipisicing
+                elit. Ipsum enim culpa nam sunt quo neque. Temporibus quisquam molestiae inventore repellat ab! Culpa in
+                repellat reprehenderit, ullam rem tempore consectetur. Praesentium!</li>
+            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">A fourth item</li>
+            <li class="list-group-item">And a fifth one</li>
+        </ul>
+    </div>
