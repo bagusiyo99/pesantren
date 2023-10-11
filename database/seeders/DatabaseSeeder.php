7e17999b844a1b3787c1fb174f1d42bc51716dfa
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-11 09:55:15 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,22 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Foto;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+ 
+public function run()
+{
+    $this->call([
+        UserSeeder::class,
+        PendaftaranSeeder::class,
+        PesanSeeder::class,
+        BlogSeeder::class,
+        FotoSeeder::class,
+    ]);
+}
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
 }
